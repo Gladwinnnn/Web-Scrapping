ca6f21fdbcba57574a761f92d4e424729369f1c8
Gladwinnnn <gladwin1503@hotmail.com>
2022-05-19 22:58:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+# os.environ['PATH'] += r"G:/Selenium Driver"
+# driver = webdriver.Chrome()
+# driver.get("https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html")
+# driver.implicitly_wait(3)
+# my_element = driver.find_element_by_id('mc-embedded-subscribe')
+# my_element.click()
+
+# method designed to wait 30s until the text has the expected text
+# WebDriverWait(driver, 30).until(
+#     EC.text_to_be_present_in_element(
+#         (By.CLASS_NAME, ), #Element filtration
+#         '' # The expected text
+#     )
+# )
 
 os.environ['PATH'] += r"G:/Selenium Driver"
 driver = webdriver.Chrome()
-driver.get("https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html")
+driver.get("https://sg.yahoo.com/?p=us")
 driver.implicitly_wait(3)
-my_element = driver.find_element_by_id('mc-embedded-subscribe')
-my_element.click()
+searchBox = driver.find_element_by_id('ybar-sbq')
+searchBox.send_keys('aws')
+# searchBox.send_keys(Keys.NUMPAD1, Keys.NUMPAD4)
 
-# method designed to wait 30s until the text has the expected text
-WebDriverWait(driver, 30).until(
-    EC.text_to_be_present_in_element(
-        (By.CLASS_NAME, ), #Element filtration
-        '' # The expected text
-    )
-)
-
+searchButton = driver.find_element_by_id('ybar-search')
+searchButton.click()
+# searchButton = driver.find_element_by_css_selector('button[class="btn btn-primary"]')
+# searchButton.click()
