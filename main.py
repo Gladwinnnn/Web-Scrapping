03d6f413bdddc6cc50afc24764fe59e396efccef
Gladwinnnn <gladwin1503@hotmail.com>
2022-06-05 22:20:22 +0800

    final

--- main.py
+++ main.py
@@ -42,6 +42,8 @@
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 from bs4 import BeautifulSoup
+import requests
+import time
 
 # with open('home.html', 'r') as html_file:
 #     content = html_file.read()
@@ -51,20 +53,46 @@ from bs4 import BeautifulSoup
 #     for course in courses_html_tags:
 #         print(course.text)
 
-with open('home.html', 'r') as html_file:
-    content = html_file.read()
+# with open('home.html', 'r') as html_file:
+#     content = html_file.read()
 
-    soup = BeautifulSoup(content, 'lxml')
-    courses_html_tags = soup.find_all('h5')
-    course_cards = soup,find_all('div', class_='card')
-    for course in course_cards:
-        course_name = course.h5.text
-        course_price = course.a.text.split()[-1]
+#     soup = BeautifulSoup(content, 'lxml')
+#     courses_html_tags = soup.find_all('h5')
+#     course_cards = soup.find_all('div', class_='card')
+#     for course in course_cards:
+#         course_name = course.h5.text
+#         course_price = course.a.text.split()[-1]
 
-        print(f'{course_name} costs {course_price}')
+#         print(f'{course_name} costs {course_price}')
 
+print('Put some skill that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
 
+def find_jobs():
+    url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation='
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
+    for index, job in enumerate(jobs):
+        published_date = job.find('span', class_='sim-posted').span.text
 
+        if "1 day" in published_date:
+            company_name = job.find('h3' , class_='joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+            more_info = job.header.h2.a['href']
 
+            if unfamiliar_skill not in skills:
+                with open(f'posts/{index}.txt', 'w') as f:
+                    f.write(f'Company Name: {company_name.strip()} \n')
+                    f.write(f'Required Skills: {skills.strip()} \n')
+                    f.write(f'More info: {more_info} \n')
+                print(f'File saved: {index}')
 
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 5
+        print(f'Waiting {time_wait} minutes...')
+        time.sleep(time_wait * 60)
